bd97590eac328513c1102d8fb1fea6aca513bb92
Michael T <ivs20172017@gmail.com>
2021-04-20 11:52:51 +0300

    3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ function App() {
   return (
     <div>
       <ReactPlayer
+        url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/1_hr_6_BROWARD_2017.mp4"
+        controls={true}
+        playsinline={true}
+        type="video/mp4"
+        config={{
+          file: {
+            attributes: {
+              disablePictureInPicture: true,
+              controlsList: "nodownload"
+            }
+          }
+        }}
+      />
+      <ReactPlayer
         url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/WC2_Broward.mp4"
         controls={true}
         playsinline={true}
