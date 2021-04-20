1b10511a8dbcda7b73c9e3c0ecfb6723a4941b66
Michael T <ivs20172017@gmail.com>
2021-04-20 09:22:34 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactPlayer from "react-player";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video
+        src="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/1_hr_6_BROWARD_2017.mp4"
+        style={{ width: "50%" }}
+        controls={true}
+      />
+      <ReactPlayer
+        url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/1_hr_6_BROWARD_2017.mp4"
+        controls={true}
+        playsinline={true}
+        type="video/mp4"
+        config={{
+          file: {
+            attributes: {
+              disablePictureInPicture: true,
+              controlsList: "nodownload"
+            }
+          }
+        }}
+      />
     </div>
   );
 }
