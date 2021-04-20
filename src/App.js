b5c11fcc458ab2bba2267debb11a5cdf763561b7
Michael T <ivs20172017@gmail.com>
2021-04-20 11:24:54 +0300

    2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import ReactPlayer from "react-player";
 function App() {
   return (
     <div>
-      <video
-        src="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/1_hr_6_BROWARD_2017.mp4"
-        style={{ width: "50%" }}
+      <ReactPlayer
+        url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/WC2_Broward.mp4"
         controls={true}
+        playsinline={true}
+        type="video/mp4"
+        config={{
+          file: {
+            attributes: {
+              disablePictureInPicture: true,
+              controlsList: "nodownload"
+            }
+          }
+        }}
       />
       <ReactPlayer
-        url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/1_hr_6_BROWARD_2017.mp4"
+        url="http://d1xcg46xsgz8e.cloudfront.net/2/instructor_repository/4072/ALABAMA_WS2.mp4"
         controls={true}
         playsinline={true}
         type="video/mp4"
